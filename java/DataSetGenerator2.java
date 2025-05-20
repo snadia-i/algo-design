93bd69417fe524b705a9eac45b32b68991e7203c
NisaaaNabilah <nisanabilah1010@gmail.com>
2025-05-20 16:51:46 +0800

    test lu

---
+++ java/DataSetGenerator2.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DataSetGenerator2 {
+    
+    
+}
